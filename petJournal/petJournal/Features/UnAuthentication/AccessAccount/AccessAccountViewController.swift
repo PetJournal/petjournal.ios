48db0ddfa70e634a897eb0838e667d842bcb3998
Marcylene Barreto <marsouza93@gmail.com>
2023-03-25 18:30:29 -0700

    [01] Feature-accessAccount: visual login screen view

---
+++ petJournal/petJournal/Features/UnAuthentication/AccessAccount/AccessAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccessAccountViewController.swift
+//  petJournal
+//
+//  Created by Marcylene Barreto on 25/03/23.
+//
+
+import UIKit
+
+class AccessAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
