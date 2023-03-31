a613108cc8a39546c2af051d1d299fd3c42a6a73
Daiane Goncalves <daigoncalves1409@gmail.com>
2023-03-31 10:29:47 -0300

    [02] Create Account screen view

---
+++ petJournal/petJournal/Features/UnAuthentication/CreateAccount/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  petJournal
+//
+//  Created by Daiane Goncalves on 30/03/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
