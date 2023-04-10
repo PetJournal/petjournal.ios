835b80880b29dc730684762bfc406a226c9c08ca
MarBarret <marsouza93@gmail.com>
2023-04-10 14:34:11 -0300

    improvement: [002] - adjustments requested by the reviewer.

--- petJournal/petJournal/Features/Auth/Home/HomeViewController.swift
+++ petJournal/petJournal/Features/Auth/Home/HomeViewController.swift
@@ -9,23 +9,19 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // TODO: how to use component
     private let textFieldPassword = PJTextField.fromXib()
     @IBOutlet weak var viewComponet: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         view.backgroundColor = .cyan
         setupViewTextFieldPassword()
-        
     }
-
 
     private func setupViewTextFieldPassword() {
         viewComponet.addSubview(textFieldPassword)
         textFieldPassword.fillSuperView()
         textFieldPassword.setupTextField(type: .securityType, title: "Password", placeholder: "your placeholder")
-//        textFieldPassword
     }
-
 }
