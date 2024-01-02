947914c6b62d3620710fe0312b6edf7728f82e86
MarBarret <marsouza93@gmail.com>
2024-01-02 20:38:15 -0300

    correção de nomeclatura de pastas, e fluxo de registro de pet

---
+++ petJournal/petJournal/Commons/CustomView/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  petJournal
+//
+//  Created by Marcylene Barreto on 02/01/24.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
---
+++ petJournal/petJournal/Features/Auth/Register Pet/InitialPatternView.swift
@@ -0,0 +1,18 @@
+//
+//  InitialPatternView.swift
+//  petJournal
+//
+//  Created by Marcylene Barreto on 02/01/24.
+//
+
+import SwiftUI
+
+struct InitialPatternView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialPatternView()
+}
