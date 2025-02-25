1499e7082f72eda3257c4c2ba9ed099aea3fbf13
Rafael Lui Seron de Paula <63885470+rafaseron@users.noreply.github.com>
2025-02-24 20:19:01 -0400

    refactor: add Spacer

--- petJournal/petJournal/Commons/Extensions/Extension+View.swift
+++ petJournal/petJournal/Commons/Extensions/Extension+View.swift
@@ -1,24 +1,18 @@
-//
-//  Extension+View.swift
-//  petJournal
-//
-//  Created by Rafael Seron on 24/02/25.
-//
-
 import SwiftUI
 
 extension View {
-    func customNavigationBar(title: String, onClick: @escaping(() -> Void) = {}) -> some View {
+    func customNavigationBar(title: String, onClick: @escaping (() -> Void) = {}) -> some View {
         return HStack {
             Button {
                 onClick()
             } label: {
                 Image(systemName: "chevron.backward")
             }
-            
+            Spacer()
             Text(title)
-
-        }
+            Spacer()
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 50, maxHeight: 50)
+            .padding(.leading)
     }
 }
 
@@ -31,4 +25,3 @@ struct CustomNavigationBarPreview: View {
 #Preview {
     CustomNavigationBarPreview()
 }
-
