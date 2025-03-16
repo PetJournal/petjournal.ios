9f6116acdebf2af3f16c25e167dfa91523001e08
Rafael Melo <rafaelnunesmelo@gmail.com>
2025-03-16 11:28:20 -0300

    Improvements and mocks to easy testing

--- petJournal/petJournal/Models/PetModel.swift
+++ petJournal/petJournal/Models/PetModel.swift
@@ -1,6 +1,8 @@
 import Foundation
+import SwiftUI
 
-struct PetModel: Codable {
+struct PetModel: Codable, Identifiable {
+    var id = UUID()
     let specieName: String
     let petName: String
     let gender: String
@@ -39,5 +41,36 @@ extension PetModel {
                  castrated: true,
                  dateOfBirth: "",
                  image: nil)
+    }
+    
+    static var mockPets = [
+        PetModel(specieName: "Cachorro", petName: "Rex", 
+                 gender: "Macho", breedName: "Vira-lata",
+                 size: "Médio", castrated: true,
+                 dateOfBirth: "01/01/2020", image: nil),
+        PetModel(specieName: "Gato", petName: "Mimi",
+                 gender: "Fêmea", breedName: "Siamês",
+                 size: "Pequeno", castrated: false,
+                 dateOfBirth: "15/05/2019", image: nil),
+        PetModel(specieName: "Cachorro", petName: "Luna",
+                 gender: "Fêmea", breedName: "Labrador",
+                 size: "Grande", castrated: true,
+                 dateOfBirth: "10/10/2018", image: nil)
+    ]
+    
+    static var mockPetImages = [
+        Image.init(asset: .banner01),
+        Image.init(asset: .logoBlue),
+        Image.init(asset: .banner02),
+        Image.init(asset: .logoPrimary),
+        Image.init(asset: .banner03)
+    ]
+    
+    func getImage() -> Image {
+        if let image = self.image, let image = UIImage(data: image) {
+            return Image(uiImage: image)
+        } else {
+            return PetModel.mockPetImages.randomElement()!
+        }
     }
 }
