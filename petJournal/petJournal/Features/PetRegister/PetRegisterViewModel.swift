65b800ff564fe7aa7fe9c8979d5d32c39ae466cb
Rafael Lui Seron de Paula <63885470+rafaseron@users.noreply.github.com>
2025-04-10 01:21:46 -0400

    feat: getBreed and getImage functions

--- petJournal/petJournal/Features/PetRegister/PetRegisterViewModel.swift
+++ petJournal/petJournal/Features/PetRegister/PetRegisterViewModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 class PetRegisterViewModel: ObservableObject {
     
@@ -9,6 +10,18 @@ class PetRegisterViewModel: ObservableObject {
     
     static let shared: PetRegisterViewModel = .init()
     
+    // MARK: - Mock de Estado para a PetRegisterView (seguindo os nomes de PetModel)
+    @Published var petName: String = ""
+    @Published var breedName: String? = nil
+    @Published var size: String = ""
+    @Published var dateOfBirth: String = ""
+    @Published var weight: String = ""
+    @Published var type: String = ""
+    @Published var gender: String = ""
+    @Published var castrated: String = ""
+    @Published var image: UIImage = UIImage(named: "banner_01")!
+    
+    // MARK: - ViewModel Functions
     func registerPet() {
         isLoading = true
         errorMessage = nil
@@ -23,6 +36,21 @@ class PetRegisterViewModel: ObservableObject {
                     self?.errorMessage = "Erro na requisição: \(error.localizedDescription)"
                 }
             }
+        }
+    }
+    
+    func getBreed() -> [String] {
+        return ["Labrador", "Lhasa Apso", "Shit Zhu", "Golden", "Sem raça definida"]
+    }
+    
+    func getImage() async {
+        do {
+            guard let imageFetched = try await AsyncImageService.asyncImage(from: "https://img.freepik.com/fotos-gratis/imagem-vertical-de-foco-raso-de-um-filhote-de-cachorro-golden-retriever-fofo-sentado-em-um-gramado_181624-27259.jpg?semt=ais_hybrid&w=740") else {
+                return
+            }
+            image = imageFetched
+        } catch {
+            image = UIImage(named: "banner_01")!
         }
     }
 }
