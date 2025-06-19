6228235d4970fe13407a2dfe07f28fd31d3fce24
Rafael Melo <rafaelnunesmelo@gmail.com>
2025-06-19 16:30:46 -0300

    Adding preview and more custom options to ServiceItemView

--- petJournal/petJournal/Features/Home/Subviews/ServiceItemView.swift
+++ petJournal/petJournal/Features/Home/Subviews/ServiceItemView.swift
@@ -1,33 +1,51 @@
-//
-//  ServiceItemView.swift
-//  petJournal
-//
-//  Created by Marcylene Barreto on 20/06/23.
-//
-
 import SwiftUI
 
 struct ServiceItemView: View {
     let service: ServiceModel
+    var itemHeight: CGFloat = 85
     
     var body: some View {
-        VStack(alignment: .center, spacing: 10) {
-            Spacer()
-            Image(service.image)
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(service.color)
-                .frame(width: 80, height: 80)
-            
-            Text(service.name)
-                .foregroundColor(service.color)
-                .font(.footnote)
-                .lineLimit(2)
-            Spacer()
+        VStack(alignment: .center, spacing: 4) {
+            serviceIcon
+            serviceName
         }
-        .frame(width: 150, height: 150)
-        .background(Color.theme.petWhite)
+        .frame(width: itemHeight, height: itemHeight)
+        .background(service.backgroundColor)
         .cornerRadius(16)
-        .shadow(radius: 10)
+        .shadow(radius: 6, x: 2, y: 2)
+        .padding(4)
+    }
+    
+    private var serviceIcon: some View {
+        Image(service.image)
+            .foregroundColor(service.color)
+            .frame(height: itemHeight / 2)
+    }
+    
+    private var serviceName: some View {
+        Text(service.name)
+            .foregroundColor(service.color)
+            .font(.robotoMedium(size: .small))
+            .lineLimit(1)
+            .truncationMode(.tail)
+            .padding(2)
+    }
+}
+
+struct ServiceItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack() {
+                ServiceItemView(service: ServiceModel(id: 0,
+                                                      name: "Todos",
+                                                      image: ImageAsset.all.rawValue,
+                                                      color: .theme.petWhite,
+                                                      backgroundColor: .theme.petPrimary500))
+                ForEach(ServiceModel.mockServices) { service in
+                    ServiceItemView(service: service)
+                }
+            }
+            .padding()
+        }.previewDisplayName("Lista de opções de serviços")
     }
 }
