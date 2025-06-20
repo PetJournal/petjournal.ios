8280e49a7cd758a6dfd326fa71fddc300d34f79b
Rafael Melo <rafaelnunesmelo@gmail.com>
2025-06-20 17:02:33 -0300

    More cases in preview and better structure onde PetButton

--- petJournal/petJournal/Commons/UIComponents/Buttons/PetButton.swift
+++ petJournal/petJournal/Commons/UIComponents/Buttons/PetButton.swift
@@ -3,7 +3,30 @@ import SwiftUI
 struct PetButton: View {
     let pet: PetModel
     var isBordered: Bool = false
+    var isSelected: Bool = false
     let action: () -> Void
+    
+    private func createBorder(isBordered: Bool) -> some View {
+        RoundedRectangle(cornerRadius: 16)
+            .stroke(isBordered ? Color.theme.petPrimary500 : .clear, lineWidth: 2)
+    }
+
+    private func createSelectionIcon(isSelected: Bool) -> some View {
+        Group {
+            if isSelected {
+                Image(asset: .pawFill)
+                    .resizable()
+                    .frame(width: 45, height: 40)
+            }
+        }
+    }
+
+    private func createOverlayContent(isBordered: Bool, isSelected: Bool) -> some View {
+        ZStack {
+            createBorder(isBordered: isBordered)
+            createSelectionIcon(isSelected: isSelected)
+        }
+    }
     
     var body: some View {
         VStack(alignment: .center, spacing: 8) {
@@ -14,8 +37,7 @@ struct PetButton: View {
                 .cornerRadius(16)
                 .clipped()
                 .overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(isBordered ? Color.theme.petPrimary500 : .clear, lineWidth: 2)
+                    createOverlayContent(isBordered: isBordered, isSelected: isSelected)
                 )
             
             Text(pet.petName)
@@ -34,19 +56,47 @@ struct PetButton_Previews: PreviewProvider {
     static var previews: some View {
         ScrollView {
             LazyVGrid(columns: gridColumns, spacing: 20) {
-                ForEach(PetModel.samplePets) { pet in
-                    PetButton(
-                        pet: pet,
-                        action: {}
-                    )
-                }
+                Text("Botões sem borda")
+                    .font(.robotoMedium(size: .small))
+                PetButton(
+                    pet: PetModel.samplePets[0],
+                    isSelected: true,
+                    action: {}
+                )
+                PetButton(
+                    pet: PetModel.samplePets[0],
+                    isSelected: false,
+                    action: {}
+                )
                 
+                Text("Botões com borda")
+                    .font(.robotoMedium(size: .small))
+                PetButton(
+                    pet: PetModel.samplePets[0],
+                    isBordered: true,
+                    isSelected: false,
+                    action: {}
+                )
+                PetButton(
+                    pet: PetModel.samplePets[0],
+                    isBordered: true,
+                    isSelected: true,
+                    action: {}
+                )
+                
+                Text("Botões de ação")
+                    .font(.robotoMedium(size: .small))
                 PetButton(
                     pet: PetModel.makePlaceholder(type: .addPet),
                     action: {}
                 )
+                PetButton(
+                    pet: PetModel.makePlaceholder(type: .allPets,
+                                                  isSelected: true),
+                    action: {}
+                )
             }
             .padding()
-        }
+        }.previewDisplayName("Variações de uso")
     }
 }
