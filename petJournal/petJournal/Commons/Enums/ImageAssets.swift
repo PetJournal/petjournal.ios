f6d2bee997e85e161088ff4ba8884d8f26999d05
Rafael Melo <rafaelnunesmelo@gmail.com>
2025-06-20 16:59:07 -0300

    Organizing enum while mapping new assets

--- petJournal/petJournal/Commons/Enums/ImageAssets.swift
+++ petJournal/petJournal/Commons/Enums/ImageAssets.swift
@@ -6,29 +6,32 @@
 //
 
 enum ImageAsset: String {
-    //Icons
-    case vaccine = "ic_vaccine"
+    // Icons
+    case addSignal = "ic_plus"
     case checkBoxClear = "ic_checkBox_clear"
     case checkBoxSelect = "ic_checkBox_select"
     case closeEye = "ic_closeEye"
+    case dogFace = "ic_dog_face"
+    case dogFood = "ic_dog_food"
     case error = "ic_error"
     case home = "ic_home"
+    case medicine = "ic_medicine"
+    case menuBurger = "menu-burger"
     case openEye = "ic_openEye"
-    case pencil = "ic_pencil"
     case paw = "ic_paw"
     case pawFill = "ic_paw_fill"
-    case user = "ic_user"
-    case vetAppointment = "ic-vet-appointment"
-    case menuBurger = "menu-burger"
+    case pencil = "ic_pencil"
+    case petSelected = "ic_pet_selected"
+    case petUnselected = "ic_pet_unselected"
     case petsCalendar = "pets_calendar"
     case petshop = "petshop"
     case registerVacine = "register_vacine"
-    case dogFood = "ic_dog_food"
-    case medicine = "ic_medicine"
-    case dogFace = "ic_dog_face"
     case shower = "ic_shower"
-    case addSignal = "ic_plus"
-    //Images
+    case user = "ic_user"
+    case vaccine = "ic_vaccine"
+    case vetAppointment = "ic-vet-appointment"
+    
+    // Images
     case banner01 = "banner_01"
     case banner02 = "banner_02"
     case banner03 = "banner_03"
@@ -40,11 +43,11 @@ enum ImageAsset: String {
     case logoLightPink = "pet_logoLightPink"
     case logoPrimary = "pet_logoPrimary"
     case logoWhite = "pet_logoWhite"
-    case petListBackground = "steps_background"
     case pet01 = "pet1"
     case pet02 = "pet2"
     case pet03 = "pet3"
     case pet04 = "pet4"
     case pet05 = "pet5"
     case pet06 = "pet6"
+    case petListBackground = "steps_background"
 }
