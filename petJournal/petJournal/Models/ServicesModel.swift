d183a870d349793f059ac2faa60e991368e11fa4
Rafael Melo <rafaelnunesmelo@gmail.com>
2025-07-14 11:48:02 -0300

    Adjustments on reusability of samples services

--- petJournal/petJournal/Models/ServicesModel.swift
+++ petJournal/petJournal/Models/ServicesModel.swift
@@ -18,22 +18,26 @@ struct ServiceModel: Identifiable, Hashable {
     }
     
     static var mockServices = [
-        ServiceModel(id: 0, name: "Vacinas",
+        ServiceModel(id: 0, name: "Todos",
+                     image: ImageAsset.all.rawValue,
+                     color: .theme.petWhite,
+                     backgroundColor: .theme.petPrimary500),
+        ServiceModel(id: 1, name: "Vacinas",
                      image: ImageAsset.vaccine.rawValue,
                      color: Color.theme.petOrange),
-        ServiceModel(id: 1, name: "Consultas",
+        ServiceModel(id: 2, name: "Consultas",
                      image: ImageAsset.vetAppointment.rawValue,
                      color: Color.theme.petGreen),
-        ServiceModel(id: 2, name: "Ração",
+        ServiceModel(id: 3, name: "Ração",
                      image: ImageAsset.dogFood.rawValue,
                      color: Color.theme.petPrimary500),
-        ServiceModel(id: 3, name: "Medicamento",
+        ServiceModel(id: 4, name: "Medicamento",
                      image: ImageAsset.medicine.rawValue,
                      color: Color.theme.petWarning500),
-        ServiceModel(id: 4, name: "Banhos",
+        ServiceModel(id: 5, name: "Banhos",
                      image: ImageAsset.shower.rawValue,
                      color: Color.theme.petSecondary500),
-        ServiceModel(id: 5, name: "Passeio",
+        ServiceModel(id: 6, name: "Passeio",
                      image: ImageAsset.dogFace.rawValue,
                      color: Color.theme.petWarning100)
     ]
